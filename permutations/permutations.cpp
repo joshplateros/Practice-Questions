bb952fb8b67f308387b3026b2d1edb0d0a159825
Joshua <josh.plateros13@gmail.com>
2020-07-08 21:52:06 -0700

    Added reverseString and permutations proto

---
+++ permutations/permutations.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> permute (std::vector<int> & nums);
+
+int main() {
+
+	std::vector<int> nums {1,2,3};
+
+
+}
+
+
+std::vector<std::vector<int>> permute (std::vector<int> & nums) {
+
+	std::vector<int> ans;
+	std::vector<int> combos;
+
+	std::sort (nums, nums + 3);
+
+		
+
+}
