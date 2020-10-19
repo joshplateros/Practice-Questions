c38041b57050a1f16aba6c4788a132c9d70e1f7e
Joshua <josh.plateros13@gmail.com>
2020-10-19 00:09:45 -0700

    Finished CamelCase

---
+++ CodeWars/camelCase/textToCamelCase.py
@@ -0,0 +1,47 @@
+
+# www.codewars.com/kata/517abf86da9663f1d2000003/train/python
+
+def to_camel_case(text):
+    splitted = []
+    result = text.find("-")
+    # completedWord = []
+    completedWord = ""
+
+
+    if (len(text) == 0):
+        return ''
+
+    # Not found
+    if (result < 0):
+        splitted = text.split("_")
+    else:
+        splitted = text.split('-')
+    
+    checkList = []   
+    # Go over each word and each letter
+    for i in range(0, len(splitted)):
+        # i is every word, [0] is the
+        # Check for Upper Camel Case
+
+        checkList.append(list(splitted[i]))
+        if (i == 0):
+            if (checkList[i][0].isupper() == True):
+                checkList[i][0] = checkList[i][0].upper()
+
+        # Fix it here
+        else:
+            if (checkList[i][0].isupper() == False):
+                checkList[i][0] = checkList[i][0].upper()
+
+        # Convert back to string
+        checkList[i] = ''.join(checkList[i])
+        
+        
+        print (checkList)
+
+
+    print ("YUP: ", completedWord.join(checkList))
+    return completedWord.join(checkList)
+
+
+to_camel_case("the_stealth_warrior")
